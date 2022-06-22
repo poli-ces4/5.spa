d4724fb2299d00e1925d2ace5bbb53af5d23cd0d
Giovanny Vargas <yovaz05@gmail.com>
2022-06-21 20:20:16 -0500

    clase 21-06-2022

---
+++ src/data/Data.js
@@ -0,0 +1,13 @@
+export const tv = {
+  topLine: "TV",
+  headLine: "Disfruta en tu TV.",
+  description:
+    "Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más.",
+  buttonLabel: "Comenzar",
+  img: "",
+  alt: "",
+  imgStart: "Start",
+  lighBackground: "true",
+  lighHeadText: "false",
+  lighDescription: "false",
+};
