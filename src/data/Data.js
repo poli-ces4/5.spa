ec81b4e9bc481e910c18b6fa72514354037b1b31
Giovanny Vargas <yovaz05@gmail.com>
2022-06-28 19:57:42 -0500

    clase 28-06-2022"

--- src/data/Data.js
+++ src/data/Data.js
@@ -7,7 +7,20 @@ export const tv = {
   img: "images/img_tv.svg",
   alt: "TV",
   imgStart: "start",
-  lighBackground: "true",
-  lighHeadText: "false",
-  lighDescription: "false",
+  lighBackground: true,
+  lighHeadText: false,
+  lighDescription: false,
+};
+
+export const series = {
+  topLine: "SERIES",
+  headLine: "Descarga tus series para verlas offline.",
+  description: "Guarda tu contenido favorito y tendrás siempre algo para ver.",
+  buttonLabel: "Comenzar",
+  img: "images/img_tv.svg",
+  alt: "TV",
+  imgStart: "start",
+  lighBackground: false,
+  lighHeadText: true,
+  lighDescription: true,
 };
